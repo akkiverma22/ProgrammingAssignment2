dcea67722c3efa50abe02fa17ef98f8882e7c154
akkiverma22 <akki.verma22@gmail.com>
2015-06-22 23:05:50 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,55 @@
 ## function to get inverse of an inversible matrix
 
 ## function to create a list of matrix , get and set method 
-
+        
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
+        ## Initialize the inverse property
+        m <- NULL
+        
+        ## Method to set the matrix
+        set <- function(y){
+                x <<- y
+                m <<- NULL
         }
-        get<-function() x
-        setmatrix<-function(solve) m<<- solve
-        getmatrix<-function() m
-        list(set=set, get=get,
-             setmatrix=setmatrix,
-             getmatrix=getmatrix)
+        
+        ## Method the get the matrix
+        get <- function() x
+        
+        ## Method to set the inverse of the matrix
+        setmatrix <- function(solve) m <<- solve
+        
+        ## Method to get the inverse of the matrix
+        getmatrix <- function() m
+        
+        ## Return a list of the methods
+        list( set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 ## calculate the inverse of the given matrix 
 ## if matrix not changed get inverse from cache
 
-cacheSolve <- function(x=matrix(), ...) {
-        m<-x$getmatrix()
-        if(!is.null(m)){
+cacheSolve <- function( x = matrix(), ...) {
+        
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        
+        ## Just return the inverse if its already set
+        if( !is.null(m) ){
                 message("getting cached data")
                 return(m)
         }
-        matrix<-x$get()
-        m<-solve(matrix, ...)
+        
+        ## Get the matrix from our object
+        matrix <- x$get()
+        
+        ## Calculate the inverse using matrix multiplication
+        m <- solve(matrix, ...)
+        
+        ## Set the inverse to the object
         x$setmatrix(m)
+        
+        ## Return the matrix
         m
 }
